178cd6f826c6b6e3f6155f8b49824240c50e6c3e
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-08-16 20:54:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 316.64 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+	public String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+
+		for (int i = 0; i < completion.length; i++) {
+			if (participant[i].equals(completion[i])) {
+				continue;
+			} else {
+				return participant[i];
+			}
+		}
+		return participant[participant.length - 1];
+	}
+}
