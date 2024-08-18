07a050402e7bd22117dc5f61994e10ce2e4b18f6
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-08-18 19:04:07 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -3,10 +3,12 @@ import java.util.Queue;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
+        // 카드1 친구들 담을 틀
         Queue<String> queue1 = new ArrayDeque<>();
+        // 카드2 친구들 담을 틀
         Queue<String> queue2 = new ArrayDeque<>();
         
-        // cards1, cards2 우선순위 큐에 추가
+        // cards1, cards2 큐에 추가
         for (String card : cards1) {
             queue1.offer(card);
         }
@@ -16,11 +18,16 @@ class Solution {
         
         // goal 배열 순서대로 확인
         for (String word : goal) {
-            if (!queue1.isEmpty() && word.equals(queue1.peek())) {
+            // 카드1친구가 안비어있고.. word가 큐의 맨 첫값(FIFO)가 같으면 빼내셈 ㅎ
+            if ( word.equals(queue1.peek())) {
                 queue1.poll();
-            } else if (!queue2.isEmpty() && word.equals(queue2.peek())) {
+            } 
+            // 카드2 친구가 안비어있고, word가 큐의 맨 첫값(FIFO)가 같으면 빼내셈 ㅎ
+            else if ( word.equals(queue2.peek())) {
                 queue2.poll();
-            } else {
+            } 
+            // 저거 안되면... 틀린거지 뭐
+            else {
                 return "No";
             }
         }
