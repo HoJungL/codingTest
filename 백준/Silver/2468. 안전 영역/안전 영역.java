370629bf06ea0ebc560a22c67be7b9a1918dae3a
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-09-05 22:35:59 +0900

    [Silver I] Title: 안전 영역, Time: 248 ms, Memory: 36564 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,82 +1,78 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.HashSet;
 import java.util.Queue;
 
 public class Main {
 
-    private static int N;
-    static int[][] matrix;
-    static boolean[][] visited;
-    static int maxSafe = 1;
+	private static int N;
+	static int[][] matrix;
+	static int[] dx = { 0, 0, 1, -1 };
+	static int[] dy = { 1, -1, 0, 0 };
+	static boolean[][] visited;
+	static int maxSafe = 1;
 
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1}; // dy[3]을 1에서 -1로 변경
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(in.readLine());
+		matrix = new int[N][N];
+		visited = new boolean[N][N];
+		int maxHeight = 0;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(in.readLine());
-        matrix = new int[N][N];
-        visited = new boolean[N][N];
-        HashSet<Integer> heights = new HashSet<>();
-        
-        int maxHeight = 0;
-        for (int i = 0; i < N; i++) {
-            String[] split = in.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
-                matrix[i][j] = Integer.parseInt(split[j]);
-                heights.add(matrix[i][j]);
-                if (matrix[i][j] > maxHeight) {
-                    maxHeight = matrix[i][j];
-                }
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			String[] split = in.readLine().split(" ");
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = Integer.parseInt(split[j]);
+				if (matrix[i][j] > maxHeight) {
+					maxHeight = matrix[i][j];
+				}
+			}
+		}
 
-        for (int rain : heights) {
-            visited = new boolean[N][N];
-            int count = 0;
+		for (int h = 0; h <= maxHeight; h++) {
+			visited = new boolean[N][N];
+			int safe = 0;
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (matrix[i][j] >= rain && !visited[i][j]) {
-                        bfs(i, j, rain);
-                        count++;
-                    }
-                }
-            }
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (matrix[i][j] > h && !visited[i][j]) {
+						safe++;
 
-            if (count > maxSafe) {
-                maxSafe = count;
-            }
-        }
+						bfs(i, j, h);
+					}
+				}
+			}
+			if (safe > maxSafe) {
+				maxSafe = safe;
+			}
+		}
+		System.out.println(maxSafe);
+	}
 
-        System.out.println(maxSafe);
-    }
+	private static void bfs(int i, int j, int height) {
+		visited[i][j] = true;
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] { i, j });
+		while (!queue.isEmpty()) {
+			int[] current = queue.poll();
+			int x = current[0];
+			int y = current[1];
+			for (int k = 0; k < 4; k++) {
+				int nx = x + dx[k];
+				int ny = y + dy[k];
+				if (isTrue(nx, ny)) {
+					if (matrix[nx][ny] > height && !visited[nx][ny]) {
+						visited[nx][ny] = true;
+						queue.add(new int[] { nx, ny });
+					}
+				}
+			}
+		}
+	}
 
-    private static void bfs(int i, int j, int rain) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[] {i, j});
-        visited[i][j] = true;
+	private static boolean isTrue(int i, int j) {
+		return i >= 0 && i < N && j >= 0 && j < N;
+	}
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-
-                if (isValid(nx, ny) && matrix[nx][ny] >= rain && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    queue.add(new int[] {nx, ny});
-                }
-            }
-        }
-    }
-
-    private static boolean isValid(int i, int j) {
-        return i >= 0 && i < N && j >= 0 && j < N;
-    }
 }
