2650d94b4eca9178ae043cff87eb6c1ed1e67f18
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-09-09 11:51:59 +0900

    [Gold III] Title: 게리맨더링, Time: 112 ms, Memory: 14964 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,12 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-	// findSet
 	static int findSet(int a) {
 		if (parents[a] == a) {
 			return a;
@@ -14,38 +14,35 @@ public class Main {
 		return parents[a] = findSet(parents[a]);
 	}
 
-	// union
 	static boolean union(int a, int b) {
 		int aRoot = findSet(a);
 		int bRoot = findSet(b);
-		if (aRoot == bRoot) {
+		if (aRoot == bRoot)
 			return false;
-		}
 		parents[bRoot] = aRoot;
 		return true;
 	}
 
-	static int[] parents;
 	static int[] population;
 	static int[][] adjMatrix;
 	static int min;
 	static int N;
+	static int[] parents;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		// 구역 수
 		N = Integer.parseInt(in.readLine());
-		// 인구수
-		String[] split = in.readLine().split(" ");
 		population = new int[N + 1];
+		// 인구수 넣고
+		String[] split = in.readLine().split(" ");
 		for (int i = 1; i <= N; i++) {
 			population[i] = Integer.parseInt(split[i - 1]);
 		}
 
 		adjMatrix = new int[N + 1][N + 1];
+		// 사람 수 + 연결된 곳
 		for (int i = 1; i <= N; i++) {
 			split = in.readLine().split(" ");
-			// 구역별 연결되어있는 구역 수 (2, 2, 4 면 2개 카운트
 			int cnt = Integer.parseInt(split[0]);
 			for (int j = 1; j <= cnt; j++) {
 				int to = Integer.parseInt(split[j]);
@@ -53,34 +50,34 @@ public class Main {
 			}
 		}
 		min = Integer.MAX_VALUE;
-		subset();
 
+		subset();
 		if (min == Integer.MAX_VALUE) {
 			System.out.println(-1);
 		} else {
 			System.out.println(min);
 		}
-
 	}
 
 	private static void subset() {
-		int count = (int) Math.pow(2, N);
-		for (int flag = 1; flag < (count / 2); flag++) {
-			List<Integer> groupA = new ArrayList<>();
-			List<Integer> groupB = new ArrayList<>();
+		int caseCount = (int) Math.pow(2, N);
+		for (int flag = 1; flag < (caseCount / 2); flag++) {
 
-			for (int i = 0; i < N; i++) {
+			List<Integer> groupA = new ArrayList<Integer>();
+			List<Integer> groupB = new ArrayList<Integer>();
+			
+			for (int i = 0; i <N; i++) {
 				if ((flag / (int) Math.pow(2, i)) % 2 == 1) {
 					groupA.add(i + 1);
 				} else {
 					groupB.add(i + 1);
 				}
 			}
-
 			parents = new int[N + 1];
 			for (int i = 0; i <= N; i++) {
 				parents[i] = i;
 			}
+
 			for (int i = 0; i < groupA.size() - 1; i++) {
 				for (int j = i + 1; j < groupA.size(); j++) {
 					int from = groupA.get(i);
@@ -90,7 +87,6 @@ public class Main {
 					}
 				}
 			}
-
 			for (int i = 0; i < groupB.size() - 1; i++) {
 				for (int j = i + 1; j < groupB.size(); j++) {
 					int from = groupB.get(i);
@@ -100,32 +96,33 @@ public class Main {
 					}
 				}
 			}
-			
 			for (int i = 0; i <= N; i++) {
 				findSet(i);
 			}
-
 			Arrays.sort(parents);
-			int cntParents = 0;
 
+			int cntParents = 0;
 			for (int i = 0; i < parents.length - 1; i++) {
 				if (parents[i] != parents[i + 1]) {
 					cntParents++;
 				}
 			}
+
 			if (cntParents == 2) {
 				int cntA = 0;
 				int cntB = 0;
 				for (int i = 0; i < groupA.size(); i++) {
-					int zone = groupA.get(i);
-					cntA += population[zone];
+					int size = groupA.get(i);
+					cntA += population[size];
 				}
 				for (int i = 0; i < groupB.size(); i++) {
-					int zone = groupB.get(i);
-					cntB += population[zone];
+					int size = groupB.get(i);
+					cntB += population[size];
 				}
+
 				min = Math.min(min, Math.abs(cntA - cntB));
 			}
 		}
 	}
+
 }
