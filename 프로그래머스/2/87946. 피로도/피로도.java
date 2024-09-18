3d894a6690a8f14935a2ec7227735c46b1f8b09f
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-09-19 08:29:59 +0900

    [level 2] Title: 피로도, Time: 2.97 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,21 @@
+class Solution {
+    static boolean[] visited;
+    static int answer = 0 ;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0,k,dungeons);
+        return answer;
+    }
+    
+    private void dfs(int depth, int stress, int[][] dungeons){  
+        for (int i = 0; i < dungeons.length; i++){  
+            if (visited[i] || dungeons[i][0] > stress) {  
+                continue;  
+            }  
+            visited[i] = true;  
+            dfs(depth + 1, stress - dungeons[i][1], dungeons);  
+            visited[i] = false;  
+        }  
+        answer = Math.max(answer, depth);  
+    }  
+}
