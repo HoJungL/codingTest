6ab73ff806a0c4e63607b17f5bba6fa15b4bc72e
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-09-22 22:18:08 +0900

    [level 2] Title: 전화번호 목록, Time: 342.28 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -5,7 +5,6 @@ class Solution {
 		boolean answer = true;
 		
 		Arrays.sort(phone_book);
-		
 		for (int i = 0 ; i<phone_book.length-1; i++) {
 			if (phone_book[i+1].startsWith(phone_book[i])) {
 				answer = false;
