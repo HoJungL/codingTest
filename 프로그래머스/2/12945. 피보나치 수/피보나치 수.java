efbb9a2b86db307951f09305c987e41059d37137
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-10-03 14:36:15 +0900

    [level 2] Title: 피보나치 수, Time: 2.93 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] fibo = new int[100001];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for(int i = 2 ; i<=n; i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2])%1234567;
+        }
+        answer = fibo[n]%1234567;
+        return answer;
+    }
+}
