e1c1ad12bc5e6d13b2f7676a3cb6544d6308b973
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-10-08 09:44:18 +0900

    [Gold IV] Title: 행성 연결, Time: 564 ms, Memory: 11816 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, a[1003][1003], lst[1003];
+vector<array<int, 3>> v;
+
+bool cmp(vector<int> a, vector<int> b) {
+    if(a[0]!=b[0]) return a[0] < b[0];
+    if( a[1]!=b[1]){
+        return a[1] < a[2];
+    }
+    return a[2] < b[2];
+	
+}
+
+int find(int x) {
+	if (x == lst[x]) return x;
+	return lst[x] = find(lst[x]);
+}
+
+void merge(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x > y) lst[x] = y;
+	else lst[y] = x;
+}
+
+void FASTIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < 1003; i++) lst[i] = i;
+    v.reserve(n * (n-1) / 2); 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+			if (i > j) {
+				v.push_back({ a[i][j], i, j });
+			}
+		}
+	}
+
+	sort(v.begin(), v.end());
+	long long res = 0;
+    
+	for (int i = 0; i < v.size(); i++) {
+		int c = v[i][0];
+		int px = v[i][1];
+		int py = v[i][2];
+		if (find(px) != find(py)) {
+			merge(px, py);
+			res += c;
+		}
+	}
+	cout << res << '\n';
+	return 0;
+}
