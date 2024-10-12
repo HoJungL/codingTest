367e7b1c7fc69c6705c8ee8294a38ac99db75ec4
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-10-12 19:57:06 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+        for(int i = 0 ; i<d.length; i++){
+
+            budget -= d[i];
+            if (budget <0){
+                break;
+            }
+            answer++;
+
+        }
+        return answer;
+    }
+}
