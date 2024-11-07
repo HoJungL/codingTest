d709eabf8a039c335db976f479a22738afda0af6
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-11-07 20:30:30 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.05 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] solution(String[][] places) {
         int[] answer = new int[5];
-        int flag;
+        int flag = 0;
 
         for (int i = 0; i < places.length; i++) {
             flag = 0;
             for (int j = 0; j < places[i].length; j++) {
-                for (int k = 0; k < places[i][j].length(); k++) {
+                for (int k = 0; k < places[j].length; k++) {
                     if (places[i][j].charAt(k) == 'P') {
                         // 가로
                         for (int l = 1; l <= 2; l++) {
@@ -17,16 +17,20 @@ class Solution {
                                 break;
                             }
                         }
-                        if (flag == 1) break;
+                        if (flag == 1) {
+                            break;
+                        }
                         for (int l = 1; l <= 2; l++) {
                             if (k - l >= 0 && places[i][j].charAt(k - l) == 'P') {
                                 flag = 1;
                                 break;
-                            } else if (k - l >= 0 && places[i][j].charAt(k - l) == 'X') {
+                            } else if (k -l >= 0 && places[i][j].charAt(k - l) == 'X') {
                                 break;
                             }
                         }
-                        if (flag == 1) break;
+                        if (flag == 1) {
+                            break;
+                        }
                         // 세로
                         for (int l = 1; l <= 2; l++) {
                             if (j + l < 5 && places[i][j + l].charAt(k) == 'P') {
@@ -36,7 +40,9 @@ class Solution {
                                 break;
                             }
                         }
-                        if (flag == 1) break;
+                        if (flag == 1) {
+                            break;
+                        }
                         for (int l = 1; l <= 2; l++) {
                             if (j - l >= 0 && places[i][j - l].charAt(k) == 'P') {
                                 flag = 1;
@@ -45,37 +51,65 @@ class Solution {
                                 break;
                             }
                         }
-                        if (flag == 1) break;
+                        if (flag == 1) {
+                            break;
+                        }
                         // 대각선1
-                        if (j + 1 < 5 && k + 1 < 5 && places[i][j + 1].charAt(k + 1) == 'P'
-                                && !(places[i][j + 1].charAt(k) == 'X' && places[i][j].charAt(k + 1) == 'X')) {
-                            flag = 1;
+                        for (int l = 1; l <= 1; l++) {
+                            if (j + l < 5 && k + l < 5 && places[i][j + l].charAt(k + l) == 'P'
+                                    &&!(places[i][j + l].charAt(k)=='X'&&places[i][j].charAt(k+l)=='X')) {
+                                    flag = 1;
+                                    break;
+                            }
+                        }
+                        if (flag == 1) {
                             break;
                         }
                         // 대각선2
-                        if (j - 1 >= 0 && k - 1 >= 0 && places[i][j - 1].charAt(k - 1) == 'P'
-                                && !(places[i][j - 1].charAt(k) == 'X' && places[i][j].charAt(k - 1) == 'X')) {
-                            flag = 1;
+                        for (int l = 1; l <= 1; l++) {
+                            if (j - l >=0 && k - l >=0 && places[i][j - l].charAt(k - l) == 'P'
+                                    &&!(places[i][j - l].charAt(k)=='X'&&places[i][j].charAt(k-l)=='X')) {
+                                    flag = 1;
+                                    break;
+                            }
+                        }
+                        if (flag == 1) {
                             break;
                         }
                         // 대각선3
-                        if (j + 1 < 5 && k - 1 >= 0 && places[i][j + 1].charAt(k - 1) == 'P'
-                                && !(places[i][j + 1].charAt(k) == 'X' && places[i][j].charAt(k - 1) == 'X')) {
-                            flag = 1;
+                        for (int l = 1; l <= 1; l++) {
+                            if (j + l < 5 && k - l >= 0 && places[i][j + l].charAt(k - l) == 'P'
+                                    &&!(places[i][j + l].charAt(k)=='X'&&places[i][j].charAt(k-l)=='X')) {
+                                    flag = 1;
+                                    break;
+                            }
+                        }
+                        if (flag == 1) {
                             break;
                         }
                         // 대각선4
-                        if (j - 1 >= 0 && k + 1 < 5 && places[i][j - 1].charAt(k + 1) == 'P'
-                                && !(places[i][j - 1].charAt(k) == 'X' && places[i][j].charAt(k + 1) == 'X')) {
-                            flag = 1;
+                        for (int l = 1; l <= 1; l++) {
+                            if (j - l >= 0 && k + l < 5 && places[i][j - l].charAt(k + l) == 'P'
+                                    &&!(places[i][j - l].charAt(k)=='X'&&places[i][j].charAt(k+l)=='X')) {
+                                    flag = 1;
+                                    break;
+                            }
+                        }
+                        if (flag == 1) {
                             break;
                         }
                     }
-                    if (flag == 1) break;
+                    if (flag == 1) {
+                        break;
+                    }
                 }
-                if (flag == 1) break;
+                if (flag == 1) {
+                    break;
+                }    
             }
-            answer[i] = (flag == 1) ? 0 : 1;
+            if (flag != 1) {
+                answer[i] = 1;
+            }
         }
         return answer;
     }
