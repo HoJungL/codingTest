56698a5c9157c1f712e6cdbbfa910ed7781745a5
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-11-07 20:31:25 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.22 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,116 +1,70 @@
+import java.util.*;
 class Solution {
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-        int flag = 0;
-
-        for (int i = 0; i < places.length; i++) {
-            flag = 0;
-            for (int j = 0; j < places[i].length; j++) {
-                for (int k = 0; k < places[j].length; k++) {
-                    if (places[i][j].charAt(k) == 'P') {
-                        // 가로
-                        for (int l = 1; l <= 2; l++) {
-                            if (k + l < 5 && places[i][j].charAt(k + l) == 'P') {
-                                flag = 1;
-                                break;
-                            } else if (k + l < 5 && places[i][j].charAt(k + l) == 'X') {
-                                break;
-                            }
+        int[] answer = new int [places.length];
+        for (int i = 0 ;i<places.length; i++){
+            String[] localHuman = places[i];
+            // System.out.print(Arrays.toString(localHuman));
+            boolean trueFalse = true;
+            for (int r = 0; r<5 && trueFalse; r++){
+                for (int c = 0 ; c<5 && trueFalse ; c++){
+                    if (localHuman[r].charAt(c) =='P'){
+                        if(!bfs(r,c,localHuman)){
+                            trueFalse = false;
                         }
-                        if (flag == 1) {
-                            break;
-                        }
-                        for (int l = 1; l <= 2; l++) {
-                            if (k - l >= 0 && places[i][j].charAt(k - l) == 'P') {
-                                flag = 1;
-                                break;
-                            } else if (k -l >= 0 && places[i][j].charAt(k - l) == 'X') {
-                                break;
-                            }
-                        }
-                        if (flag == 1) {
-                            break;
-                        }
-                        // 세로
-                        for (int l = 1; l <= 2; l++) {
-                            if (j + l < 5 && places[i][j + l].charAt(k) == 'P') {
-                                flag = 1;
-                                break;
-                            } else if (j + l < 5 && places[i][j + l].charAt(k) == 'X') {
-                                break;
-                            }
-                        }
-                        if (flag == 1) {
-                            break;
-                        }
-                        for (int l = 1; l <= 2; l++) {
-                            if (j - l >= 0 && places[i][j - l].charAt(k) == 'P') {
-                                flag = 1;
-                                break;
-                            } else if (j - l >= 0 && places[i][j - l].charAt(k) == 'X') {
-                                break;
-                            }
-                        }
-                        if (flag == 1) {
-                            break;
-                        }
-                        // 대각선1
-                        for (int l = 1; l <= 1; l++) {
-                            if (j + l < 5 && k + l < 5 && places[i][j + l].charAt(k + l) == 'P'
-                                    &&!(places[i][j + l].charAt(k)=='X'&&places[i][j].charAt(k+l)=='X')) {
-                                    flag = 1;
-                                    break;
-                            }
-                        }
-                        if (flag == 1) {
-                            break;
-                        }
-                        // 대각선2
-                        for (int l = 1; l <= 1; l++) {
-                            if (j - l >=0 && k - l >=0 && places[i][j - l].charAt(k - l) == 'P'
-                                    &&!(places[i][j - l].charAt(k)=='X'&&places[i][j].charAt(k-l)=='X')) {
-                                    flag = 1;
-                                    break;
-                            }
-                        }
-                        if (flag == 1) {
-                            break;
-                        }
-                        // 대각선3
-                        for (int l = 1; l <= 1; l++) {
-                            if (j + l < 5 && k - l >= 0 && places[i][j + l].charAt(k - l) == 'P'
-                                    &&!(places[i][j + l].charAt(k)=='X'&&places[i][j].charAt(k-l)=='X')) {
-                                    flag = 1;
-                                    break;
-                            }
-                        }
-                        if (flag == 1) {
-                            break;
-                        }
-                        // 대각선4
-                        for (int l = 1; l <= 1; l++) {
-                            if (j - l >= 0 && k + l < 5 && places[i][j - l].charAt(k + l) == 'P'
-                                    &&!(places[i][j - l].charAt(k)=='X'&&places[i][j].charAt(k+l)=='X')) {
-                                    flag = 1;
-                                    break;
-                            }
-                        }
-                        if (flag == 1) {
-                            break;
-                        }
-                    }
-                    if (flag == 1) {
-                        break;
                     }
                 }
-                if (flag == 1) {
-                    break;
-                }    
             }
-            if (flag != 1) {
+            if (trueFalse == true){
                 answer[i] = 1;
+            }else{
+                answer[i] = 0;
             }
+
         }
         return answer;
+    }
+    
+    public static boolean bfs(int x, int y, String[] human) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[5][5];
+        queue.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            // 뽑아내고
+            int[] position = queue.poll();
+            // 움직인 위치
+            int ex = position[0];
+            int ey = position[1];
+            // 최대 거리가 2이상이라며... 맨허튼 length
+            int len = Math.abs(ex - x) + Math.abs(ey - y);
+
+            // len이 0이상, 2이하! 그리고 사람의 현재 위치가 P이면...
+            if (len > 0 && human[ex].charAt(ey) == 'P' && len <= 2) {
+                return false;
+            }
+
+            if (len < 2) {
+                for (int i = 0; i < 4; i++) {
+                    int nx = ex + dx[i];
+                    int ny = ey + dy[i];
+
+                    // 갈수 있고!, 방문 안했고!, X가 아니면!
+                    if (isValue(nx, ny) && !visited[nx][ny] && human[nx].charAt(ny) != 'X') {
+                        // queue에 너어주세요
+                        queue.add(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
+    public static boolean isValue(int i, int j){
+        return i>=0 && i <5 && j>=0 && j<5;
     }
 }
