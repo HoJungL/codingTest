69c57a1cc0b5236805e5c00a14173e39223e0acb
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2024-11-14 19:35:00 +0900

    [level 3] Title: 숫자 게임, Time: 110.01 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,19 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
-        int answer = 0;
         Arrays.sort(A);
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int b : B) {
-            queue.add(b);
-        }
-        for (int i = 0; i < A.length; i++) {
-            while (!queue.isEmpty() && queue.peek() <= A[i]) {
-                queue.poll();
-            }
-            if (!queue.isEmpty()) {
-                queue.poll();
-                answer++;
+        Arrays.sort(B);
+
+        int answer = 0;
+        int i = 0;
+        int j = 0;
+
+        for (;i<A.length;i++){
+            for( ;j<B.length;j++){
+                if (A[i]<B[j]){
+                    answer++;
+                    j++;
+                    break;
+                }
             }
         }
         return answer;
