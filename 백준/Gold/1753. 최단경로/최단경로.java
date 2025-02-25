5a48ee0deed1373a55bf247fb952b13435cf0916
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2025-02-25 21:56:57 +0900

    [Gold IV] Title: 최단경로, Time: 584 ms, Memory: 103560 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,95 +1,86 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-
 public class Main {
-
-	private static class Node {
-		public int vertex;
-		public Node link;
-		public int weight;
-
-		public Node(int vertex, Node link, int weight) {
-			super();
-			this.vertex = vertex;
-			this.link = link;
-			this.weight = weight;
-		}
-
-		@Override
-		public String toString() {
-			return "Node [vertex=" + vertex + ", link=" + link + ", weight=" + weight + "]";
-		}
-
-	}
-
-	private static final int INF = Integer.MAX_VALUE;
-
-	private static int V; // 정점의 개수
-	private static int E; // 간선의 개수
-
-	public static void main(String[] args) throws Exception {
-		// 데이터 받고
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		// 데이터 나누고
-		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-
-		// 출발 정점 입력하고
-		int start = Integer.parseInt(in.readLine());
-
-		// 정점 수만큼 Node 배열 만들고
-		Node[] adjList = new Node[V + 1];
-
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(in.readLine(), " ");
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-
-			adjList[from] = new Node(to, adjList[from], weight);
-		}
-
-		int[] distance = new int[V + 1];
-		boolean[] visited = new boolean[V + 1];
-		Arrays.fill(distance, INF);
-
-		distance[start] = 0;
-
-		int min = 0;
-		int current = 0;
-
-		for (int cnt = 0; cnt < V; cnt++) {
-			current = -1;
-			min = INF;
-
-			for (int i = 1; i <= V; i++) {
-				if (!visited[i] && distance[i] < min) {
-					min = distance[i];
-					current = i;
-				}
-			}
-
-			// (선택사항) 가지치기 : 정점을 못 찾은 경우는 종료
-			if (current == -1) {
-				break;
-			}
-			visited[current] = true;
-
-			for (Node temp = adjList[current]; temp != null; temp = temp.link) {
-				if (!visited[temp.vertex] 
-						&& distance[temp.vertex] > min + temp.weight) {
-					distance[temp.vertex] = min + temp.weight;
-				}
-			}
-
-		}
-		for (int i = 1; i < distance.length; i++) {
-			System.out.println(distance[i] != INF ? distance[i] : "INF");
-		}
-	}
-
+    private static class Node {
+        int vertex;
+        int weight;
+        Node next;
+        public Node(int vertex, int weight, Node next) {
+            this.vertex = vertex;
+            this.weight = weight;
+            this.next = next;
+        }
+    }
+    
+    private static class Pair implements Comparable<Pair> {
+        int vertex, distance;
+        public Pair(int vertex, int distance) {
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+        @Override
+        public int compareTo(Pair other) {
+            return Integer.compare(this.distance, other.distance);
+        }
+    }
+    
+    private static final int INF = Integer.MAX_VALUE;
+    private static int V, E;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        
+        int start = Integer.parseInt(in.readLine());
+        Node[] adjList = new Node[V + 1];
+        
+        // 인접리스트 생성 (각 정점에 대해 연결리스트 형태)
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(in.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            
+            // head 삽입
+            adjList[from] = new Node(to, weight, adjList[from]);
+        }
+        
+        int[] distance = new int[V + 1];
+        Arrays.fill(distance, INF);
+        distance[start] = 0;
+        
+        // 우선순위 큐를 이용한 다익스트라 알고리즘
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.offer(new Pair(start, 0));
+        
+        while (!pq.isEmpty()) {
+            Pair current = pq.poll();
+            int u = current.vertex;
+            
+            // 이미 더 짧은 경로가 발견된 경우 스킵
+            if (current.distance > distance[u]) continue;
+            
+            for (Node temp = adjList[u]; temp != null; temp = temp.next) {
+                int v = temp.vertex;
+                int newDist = distance[u] + temp.weight;
+                if (newDist < distance[v]) {
+                    distance[v] = newDist;
+                    pq.offer(new Pair(v, newDist));
+                }
+            }
+        }
+        
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            sb.append(distance[i] == INF ? "INF" : distance[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
