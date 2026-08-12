ff317d2a45c6034e921fd4045be845b29b990015
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-08-12 10:11:39 +0900

    [level 0] Title: 약수 구하기, Time: 0.33 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int n) {
+        int count =0;
+        for (int i = 1; i<=n;i++){
+            if (n % i ==0){
+                count +=1;
+            }
+        }
+        int[] answer = new int[count];
+        int index = 0;
+        for (int i = 1 ; i <=n;i++){
+            if (n % i ==0)
+            answer[index++]= i;
+        }
+        return answer;
+    }
+}
