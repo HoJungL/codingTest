dc297d4be1e7399fd2204071663e7812128f260a
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-08-12 11:12:17 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        
+        // 2배..
+        for (int i = 0 ; i<t;i++){
+           n *=2;
+        }
+        answer = n;
+        return answer;
+    }
+}
