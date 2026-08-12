30a015d796d0ddc0a59310dac85df31ddcca4d68
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-08-12 11:07:39 +0900

    [level 0] Title: 제곱수 판별하기, Time: 1.77 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        if (Math.sqrt(n) % 1 == 0){
-            answer =1;
-        } else{
-            answer =2;
+        int answer = 2;
+        for(int i=1; i<n/2; i++){
+            if(n/i == i && n%i == 0){
+                answer = 1;
+                break;
+            }
         }
         return answer;
     }
