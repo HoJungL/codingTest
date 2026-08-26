ec3bde97b2f411e18f39bc4c0804b88ee1707e3d
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-08-26 11:30:35 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.07 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        if (my_string.contains(target)){
+            return 1;
+        }
+        return 0;
+    }
+}
