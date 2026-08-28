d7b13be3a882709984688a6f927f73ed71b01c11
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-08-28 09:46:29 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        for (int i = 0 ; i <numbers.length;i++){
+            if (answer <=n){
+                answer += numbers[i];
+            }else {
+                        return answer;
+
+            }
+        }
+        return answer;
+    }
+}
