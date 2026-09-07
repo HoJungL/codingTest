8cfbb6203fb040bff73007be6b9731a3d177956a
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-07 18:49:30 +0900

    [Passed] 두줄 출력 (87ms, 9MB)

---
+++ trail1/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.println("Hello students!");
+        System.out.println("Welcome to LeebrosCode!");
+
+    }
+}
