9f67be663a2eb04f88c34ad2cbbe12eee3b1d93b
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-07 18:51:51 +0900

    [Passed] 변수 선언하기 (90ms, 9MB)

---
+++ trail1/변수 선언하기/declaring-variables.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a= 3;
+        String b = "C";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
