8f839447147e441c705e323a4bd46ea56b94c835
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-10 17:33:16 +0900

    [Passed] 입력받아 계산 4 (90ms, 9MB)

---
+++ trail1/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(2 * n);
+    }
+}
