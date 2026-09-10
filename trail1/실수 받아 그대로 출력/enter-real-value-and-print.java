2c9ea49e000ea522d66e346a12213071591750eb
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-10 17:38:52 +0900

    [Passed] 실수 받아 그대로 출력 (100ms, 9MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double n = Double.parseDouble(br.readLine());
+        System.out.printf("%.2f", n);
+        
+    }
+}
