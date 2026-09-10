c393c8ec59e33372c80f85f14f42a3c65bcc85f4
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-10 14:12:45 +0900

    [Passed] 변수 값 교체하기 5 (91ms, 9MB)

---
+++ trail1/변수 값 교체하기 5/replacing-variable-values-5.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.println(6);
+        System.out.println(6);
+        
+    }
+}
