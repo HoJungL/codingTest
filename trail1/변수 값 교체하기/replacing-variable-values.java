db72e9a5c38551ea05961ba9caaee32832be59bf
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-10 14:11:06 +0900

    [Passed] 변수 값 교체하기 (88ms, 9MB)

---
+++ trail1/변수 값 교체하기/replacing-variable-values.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        a = 6;
+        System.out.println(a);
+    }
+}
