b4dcff55442aa05019d1581e0627944d0e3fdf9e
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-10 14:11:59 +0900

    [Passed] 문자 변경하기 (94ms, 9MB)

---
+++ trail1/문자 변경하기/change-charater.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String a = "C";
+        a = "T";
+        System.out.println(a);
+    }
+}
