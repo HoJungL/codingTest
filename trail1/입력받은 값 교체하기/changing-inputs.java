8945b26ad95eb3ca2d2979ec274011d176552679
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-15 09:20:04 +0900

    [Passed] 입력받은 값 교체하기 (99ms, 9MB)

---
+++ trail1/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
+
+    }
+}
