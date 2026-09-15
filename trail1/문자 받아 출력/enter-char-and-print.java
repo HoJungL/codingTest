f387957d535013ac30d74960145e5ea68d8ae6d8
Ho-Jung Lee <105427432+HoJungL@users.noreply.github.com>
2026-09-15 11:36:02 +0900

    [Passed] 문자 받아 출력 (93ms, 9MB)

---
+++ trail1/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        System.out.println(a);
+    }
+}
